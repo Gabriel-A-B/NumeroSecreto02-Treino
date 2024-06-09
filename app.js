dc78954e34de9a81c744dcdbf4d2f7d75862db6e
GabrielAndrade <91852179+Gabriel-A-B@users.noreply.github.com>
2024-06-09 19:08:08 -0300

    up

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+let paragrafo = document.querySelector('p')
+let resposta = document.querySelector('.container__input')
+titulo.innerHTML = `Jogo do numero Secreto`
+paragrafo.innerHTML = `tente adivinhar o numero secreto`
+
+function verificarChute() {
+    
+}
