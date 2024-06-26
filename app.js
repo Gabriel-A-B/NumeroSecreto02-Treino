593e9067d9a5ec4bb55273ab9e783793a588b45e
GabrielAndrade <91852179+Gabriel-A-B@users.noreply.github.com>
2024-06-26 20:20:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let maior = 'maior'
 let menor = 'menor'
-let tentativas = 0;
+let tentativas = 1;
 
 let aleatorio = numeroAleatorio();
 console.log(aleatorio);
@@ -8,6 +8,7 @@ console.log(aleatorio);
 function construirSite(tag, texto) {
     let corpo = document.querySelector(tag);
     corpo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 construirSite('h1', 'Jogo Do Numero Secreto');
@@ -15,28 +16,28 @@ construirSite('p', 'chute um numero de 1 a 100');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    while (chute != aleatorio) {
-        console.log('lopp')
-        //caso n for um numero
-        if (!isNaN(chute) || Number.isInteger(chute)) {
-            construirSite('p', 'por favor coloque um numero valido, de 1 a 100');
-        //caso ele erre
-        }else {
+    //caso o umero esteja certo
+    if (chute == aleatorio) {
+       construirSite('h1', 'Parabens voce ganhou!!!');
+       var vezes = 1 ? vezes = 'tentativa' : vezes = 'tetativas'
+       construirSite('p', `voce acertou com ${tentativas} ${vezes}`);
+       var resposta = document.querySelector('.container__input').style.display = 'none';
+       var chutar = document.querySelector('.chutar').setAttribute('disabled', 'true');
+       var Novogame = document.querySelector('#reiniciar').removeAttribute('disabled', 'true');
+    //caso ele erre
+    }else if(chute == ''){
+        construirSite('p', `Por Favor prencha o campo de resposta, com uma resposta valida`);
+    
+    }else if(chute > 100 || chute < 0){
+        construirSite('p', `Sua resposta deve ser entre 0 a 100`);
+    
+    }else{
             tentativas++;
             let dica = '';
             chute < aleatorio ? dica = maior : dica = menor;
             construirSite('h1', 'Voce errou tente novamente');
             construirSite('p', `O numero Aleatorio e ${dica}`);
-            console.log(chute);
-        }
-    }
-    //caso o umero esteja certo
-    if (chute == aleatorio) {
-       construirSite('h1', 'Parabens voce ganhou!!!');
-       construirSite('p', `voce acertou com ${tentativas} tentativas`);
-       var resposta = document.querySelector('.container__input').style.display = 'none';
-       var chutar = document.querySelector('.chutar').setAttribute('disabled', 'true');
-       var Novogame = document.querySelector('#reiniciar').removeAttribute('disabled', 'true');
+            limparChute() 
     }
 }
 
@@ -46,4 +47,9 @@ function novoGame() {
 
 function numeroAleatorio() {
     return parseInt(Math.random() * 100 + 1);   
+}
+
+function limparChute() {
+    chute = document.querySelector('input');
+    return chute.value = '';
 }
